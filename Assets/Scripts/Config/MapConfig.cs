12f416ad5374d1c21a41204a727ace158cd040eb
ayegnag <gangeya.u@gmail.com>
2023-02-19 19:13:50 +0530

    Refactored to use new MapSystem

--- Assets/Scripts/Config/MapConfig.cs
+++ Assets/Scripts/Config/MapConfig.cs
@@ -4,5 +4,14 @@ namespace KOI
     {
         public static int WorldMapWidth = 250;
         public static int WorldMapHeight = 150;
+        
+        public static int Seed = 454530;   // 0 to 100000000
+        public static float NoiseScale = 0.55f;
+        public static int NoiseOctaves = 8; // 1 to 10
+
+        public static int ChanceGroundTree = 6; // 1 to 10; 6 = 40% chance
+        public static int ChanceBeachTree = 8; // 1 to 10; 8 = 20% chance
     }
 }
+
+// 454530 -  4 Islands
